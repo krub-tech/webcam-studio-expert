a7bada79d0475ad2ab64c1bc131b689996c967e7
p.golik <p.golik@aitarget.com>
2021-05-10 19:44:49 +0300

    set CSRFTOKEN in headers

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,16 @@
 import axios from 'axios';
+import Cookie from 'js-cookie';
 
 export const client = axios.create({
   baseURL: process.env.VUE_APP_BASE_API_URL,
+});
+
+client.interceptors.request.use((config) => {
+  if (config.method !== 'get') {
+    config.headers['X-CSRFTOKEN'] = Cookie.get('csrftoken');
+  }
+
+  return config;
 });
 
 export function postFormData(data) {

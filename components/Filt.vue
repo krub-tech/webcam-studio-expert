aca2e8a2ce9a79886dee0a609f170ea8ae02a7da
p.golik <p.golik@aitarget.com>
2021-05-18 13:18:41 +0300

    fix events

--- components/Filt.vue
+++ components/Filt.vue
@@ -21,7 +21,7 @@
           :key="key"
           :item="studio_type"
           :checked="isChecked($store.state.filter.params.studio_type, key)"
-          @click="checkboxHandle(key, 'studio_type')"
+          @mouseup.native="checkboxHandle(key, 'studio_type')"
         />
       </div>
       <hr />
@@ -33,7 +33,7 @@
             :item="models_age"
             :selector="'models_age'"
             :selected="$store.state.filter.params.models_age.toString()"
-            @click="radioHandle(key, 'models_age')"
+            @mouseup.native="radioHandle(key, 'models_age')"
           />
         </div>
       </div>
@@ -46,7 +46,7 @@
           :checked="
             isChecked($store.state.filter.params.working_with_model_types, key)
           "
-          @click="checkboxHandle(key, 'working_with_model_types')"
+          @mouseup.native="checkboxHandle(key, 'working_with_model_types')"
         />
       </div>
       <hr />

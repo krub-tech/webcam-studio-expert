118c1cab1de57628771dc82d850757e7d93507e6
Dima Glushkov <dglushkov@mail.ru>
2021-07-01 00:52:01 +0700

    links index title add

--- pages/links/index.vue
+++ pages/links/index.vue
@@ -1,9 +1,15 @@
 <template>
-  <LinksSidebar
-    class="links--nav-main"
-    :options="$store.state.links.options"
-    :categories="categories"
-  />
+  <div>
+    <h1 class="mobile">
+      Полезные ссылки для<br />
+      вебкам-моделей
+    </h1>
+    <LinksSidebar
+      class="links--nav-main"
+      :options="$store.state.links.options"
+      :categories="categories"
+    />
+  </div>
 </template>
 
 <script>
@@ -22,7 +28,21 @@ export default {
 </script>
 
 <style lang="scss">
+h1.mobile {
+  font-size: 18px;
+  line-height: 23px;
+  text-align: center;
+  margin: 28px 20px;
+}
 .links--nav-main {
   margin-top: 30px;
+}
+@media screen and (min-width: 420px) {
+  h1.mobile {
+    text-align: left;
+    br {
+      display: none;
+    }
+  }
 }
 </style>
